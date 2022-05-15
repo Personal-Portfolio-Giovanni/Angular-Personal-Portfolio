bf59836014c0efb4b8420d7b0d7fd63dc323a17a
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2022-05-15 16:58:36 +0200

    Added environment url link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   baseUrl: 'https://giovannilamarmoraproject.github.io/Portfolio/',
   googleID: 'G-0VT47TDXGS',
+  profileImageUrl:
+    'https://media-exp1.licdn.com/dms/image/C4D03AQH8e_2qWvDl9g/profile-displayphoto-shrink_800_800/0/1620846625034?e=1658361600&v=beta&t=shqP-k9GU6x34mWzIw4mERigXygTV3vTCf4WSoNn0Yw',
 };
