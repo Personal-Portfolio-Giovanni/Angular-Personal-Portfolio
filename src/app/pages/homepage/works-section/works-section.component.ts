881bdd4a8fc1bdd16d53b3e78f16ad6fb9887d09
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2023-02-13 16:55:07 +0100

    feat(): Contentful CMS

--- src/app/pages/homepage/works-section/works-section.component.ts
+++ src/app/pages/homepage/works-section/works-section.component.ts
@@ -1,21 +1,42 @@
-import { Component, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import {
   ClassType,
   WorkType,
 } from 'src/app/shared/class/accordion-constant.class';
-import { WorkInterface } from 'src/app/shared/interfaces/work.interface';
+import { CMSData } from 'src/app/shared/class/colorful.class';
+import { ContentfulService } from 'src/app/shared/services/contentful.service';
 
 @Component({
   selector: 'app-works-section',
   templateUrl: './works-section.component.html',
   styleUrls: ['./works-section.component.css'],
 })
-export class WorkSectionComponent {
-  @Output('works') works: Array<WorkInterface> = [];
+export class WorkSectionComponent implements OnInit {
+  @Input('works') works: Array<CMSData> = [];
   @Output('code') code: string = WorkType.CODE;
   @Output('parentID') parentID: string = WorkType.PARENTID;
 
   public get classType(): typeof ClassType {
     return ClassType;
+  }
+  constructor(private contentService: ContentfulService) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      if (
+        this.contentService.worksData &&
+        this.contentService.worksData.length > 0
+      ) {
+        this.works = this.contentService.worksData;
+      }
+    }, 500);
   }
 }
---
+++ src/app/shared/services/contentful.service.ts
@@ -0,0 +1,40 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+import { CMSData, Contentful } from '../class/colorful.class';
+import {
+  EmailResponseModel,
+  EmailSenderModel,
+  EmailTemplateModel,
+} from '../class/emailSender.class';
+import { ErrorResponse } from '../class/error.class';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContentfulService {
+  environment = environment;
+  public worksData: CMSData[] = [];
+  public courseData: CMSData[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getCMSData(locale: string): Observable<any> {
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer _CF0iFOve3oKWY63R9EhELmLrLsPGSzMBCz9X9EISe4',
+    });
+
+    const url =
+      environment.contentfulBaseUrl +
+      environment.worksUrl +
+      '?locale=' +
+      locale;
+    return this.http.get(url, { headers: headers });
+  }
+}
