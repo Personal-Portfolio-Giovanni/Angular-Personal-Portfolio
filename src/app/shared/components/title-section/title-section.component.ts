b311b88d0584319c8ff1cb8a2835278b091678fd
Giovanni <giovannilamarmora.working@gmail.com>
2024-07-15 18:40:57 +0200

    feat(): Bug Fix, and added footer

--- src/app/shared/components/title-section/title-section.component.ts
+++ src/app/shared/components/title-section/title-section.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AnimationsService } from '../../services/config/animation.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title-section',
@@ -10,14 +9,4 @@ export class TitleSectionComponent {
   @Input('class') class: string = '';
   @Input('title') title: string = '';
   @Input('sub_title') sub_title: string = '';
-  animation_title: string = '';
-
-  constructor(public animationService: AnimationsService) {}
-
-  changeAnimation() {
-    this.animation_title = this.animationService.animation_title;
-    document.querySelectorAll('#title_section').forEach((element) => {
-      element.setAttribute('data-inviewport', this.animation_title);
-    });
-  }
 }
